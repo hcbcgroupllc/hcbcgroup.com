75095335cd422fe91666e56581fb785a41e6fde2
hcbcggithub <skychoi@hcbcgroup.com>
2024-12-30 00:29:49 -0500

    set: new static site - temporary

---
+++ alert.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded",() => {
+    alert(`
+        Welcome.\n
+        Our site is currently undergoing a new design setup.\n
+        Please check back in January 2025.
+        Sorry for the inconvenience.
+        `)
+})
