280ae178bb1eaf6231a09198c5276c3d1e15ddc7
hcbcggithub <skychoi@hcbcgroup.com>
2024-12-31 18:25:31 -0500

    update: update site page construction

--- alert.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded",() => {
-    alert(`
-        Welcome.\n
-        Our site is currently undergoing a new design setup.\n
-        Please check back in January 2025.
-        Sorry for the inconvenience.
-        `)
-})
